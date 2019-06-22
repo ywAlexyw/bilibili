9b3ef2d0173cb3ab2a0ddde56187c58d30301eb9
unknown <545746172@qq.com>
2019-06-22 16:48:07 +0800

    fff

--- src/views/rank.vue
+++ src/views/rank.vue
@@ -55,7 +55,7 @@
                                 </div>
                                 <div class="comments">
                                     <img src="//s1.hdslb.com/bfs/static/mult/images/ico_danmu.png">
-                                    <p>{{item.asdasd}}</p>
+                                    <p>{{item.asdadasdasd}}</p>
                                 </div>
                             </div>
                         </div>
